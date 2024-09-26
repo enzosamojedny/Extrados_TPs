046dddf91136eee49cc2246329d4f27e69fd8cc0
enzosamojedny <enzosamojedny@gmail.com>
2024-09-26 10:33:02 -0300

    mejoras generales en el codigo

--- Program.cs
+++ Program.cs
@@ -10,8 +10,12 @@ namespace Extrados_txt_reader
             string txtLocaciones = "locaciones.txt";
             string txtEmpleados = "empleados.txt";
             string txtDepartamentos = "departamentos.txt";
+
+            Console.WriteLine("Seleccione la DB que quiera agregar a mssql");
+            var dbSeleccionada = Console.ReadLine().ToLower() + ".txt";
+            
             string connectionString = "Server=ADMIN;Database=ExtradosDB;Trusted_Connection=True;TrustServerCertificate=True;";
-            using (StreamReader reader = new StreamReader(txtDepartamentos))
+            using (StreamReader reader = new StreamReader(dbSeleccionada))
             {
                 string linea;
                 List<string> lista = new List<string>();
@@ -24,19 +28,21 @@ namespace Extrados_txt_reader
                     if (linea.StartsWith('|'))
                     {
                         linea = linea.Trim('|');//saco los | del principio
-                        
                         string[] columnas = linea.Split('|'); //separo las columnas por |
-                        
-                        for (int i = 0; i <columnas.Length; i++)
+
+                        for (int i = 0; i < columnas.Length; i++)
                         {
-                            if (string.IsNullOrWhiteSpace(columnas[i]))
+                            if (columnas[i]?.Trim() == "0") //si hay un 0, lo seteo a NULL -- no esta funcionando esto
+                            {
+                                columnas[i] = "NULL";
+                            }
+                            if (string.IsNullOrWhiteSpace(columnas[i])) //si hay espacios vacios, los seteo a NULL
                             {
                                 columnas[i] = "NULL";
                             }
                             else
                             {
-                                columnas[i] = columnas[i].Trim();
-                                
+                                columnas[i] = columnas[i].Trim(); 
                             }
                         }
                         string resultado = string.Join(",", columnas);
@@ -44,22 +50,38 @@ namespace Extrados_txt_reader
                         lista.Add(resultado);
                     }
                 }
-                //Console.WriteLine($"INSERT INTO locaciones ({lista[0]}) " + $"VALUES ({lista[1]})");
-                string[] values = lista[1].Split(',');
+                lista.Remove(lista[0]);
+                List<string> listaDeValores = new List<string>();//elimino el titulo de las tablas
 
+                
                 //problema a solucionar: el tipo de dato dentro de los insert es string...
                 //foreach (var item in values)
                 //{
                 //    Console.WriteLine($"Value: {item.Trim()}, Type: {item.GetType()}");
                 //}
+
                 string locacionQuery;
                 foreach (var row in lista)
                 {
-                    
-
-                    locacionQuery = $"INSERT INTO departamentos ({lista[0]}) " + $"VALUES ({row})";
-                    Console.WriteLine(locacionQuery);
-                    //Console.WriteLine(locacionQuery);
+                    if (dbSeleccionada == "locaciones.txt")
+                    {
+                        locacionQuery = $"INSERT INTO locaciones ( ID_LOCACION, DIRECCION , CODIGO_POSTAL, CIUDAD, ESTADO_PROVINCIA, ID_PAIS) " + $"VALUES ({row})";
+                        Console.WriteLine(locacionQuery);
+                    }
+                    else if (dbSeleccionada == "departamentos.txt")
+                    {
+                        locacionQuery = $"INSERT INTO departamentos (ID_DEPARTAMENTO, NOMBRE, ID_MANAGER, ID_LOCACION) " + $"VALUES ({row})";
+                        Console.WriteLine(locacionQuery);
+                    }
+                    else if (dbSeleccionada == "empleados.txt")
+                    {
+                        locacionQuery = $"INSERT INTO empleados (ID_EMPLEADO, NOMBRE, APELLIDO, EMAIL, TELEFONO, FECHA_EN, ID_TRABAJO, SALARIO, COMISION, ID_MANAGER, ID_DEPARTAMENTO) " + $"VALUES ({row})";
+                        Console.WriteLine(locacionQuery);
+                    }
+                    else {
+                        Console.WriteLine("La DB no existe o ingresaste mal el nombre");
+                        return;
+                    }
                 }
                 //        using (SqlCommand command = new SqlCommand(locacionQuery, connection))
 
