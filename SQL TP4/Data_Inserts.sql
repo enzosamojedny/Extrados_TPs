4b35ed9e632962e7399c8cd69bad09d20d8295e5
enzosamojedny <enzosamojedny@gmail.com>
2024-10-16 02:19:17 -0300

    Actividad 3 TP4 terminada

--- SQL TP4/Data_Inserts.sql
+++ SQL TP4/Data_Inserts.sql
@@ -1,16 +1,14 @@
 -- REGISTRO DE ADMINISTRADORES
-INSERT INTO administradores (DNI, nombre, nombre_usuario, contraseña,  fecha_baja)
-VALUES ('43.123.432', 'Juan Pérez', 'juan.perez', 'contraseñaSegura123', NULL);
-INSERT INTO administradores (DNI, nombre, nombre_usuario, contraseña, fecha_baja)
-VALUES ('21.563.213', 'Ana García', 'ana.garcia', 'otraContraseñaSegura456', NULL);
+INSERT INTO administradores (DNI, nombre, nombre_usuario, contraseÃ±a,  fecha_baja)
+VALUES ('43.123.432', 'Juan PÃ©rez', 'juan.perez', 'contraseÃ±aSegura123', NULL);
+INSERT INTO administradores (DNI, nombre, nombre_usuario, contraseÃ±a, fecha_baja)
+VALUES ('21.563.213', 'Ana GarcÃ­a', 'ana.garcia', 'otraContraseÃ±aSegura456', NULL);
 
 -- REGISTRO DE RECEPCIONISTAS
-INSERT INTO recepcionistas (DNI, nombre, nombre_usuario, contraseña, id_creador)
-VALUES ('23.432.123', 'Carlos López', 'carlos.lopez', 'password123', '21.563.213');
-INSERT INTO recepcionistas (DNI, nombre, nombre_usuario, contraseña, id_creador)
-VALUES ('56.453.213', 'María Fernández', 'maria.fernandez', 'securePass456', '43.123.432');
-INSERT INTO recepcionistas (DNI, nombre, nombre_usuario, contraseña, id_creador)
-VALUES ('13.632.113', 'Jose Passerini', 'jose.passerini', 'password123', '21.563.213');
+INSERT INTO recepcionistas (DNI, nombre, nombre_usuario, contraseÃ±a, id_creador)
+VALUES ('33.456.789', 'Carlos LÃ³pez', 'carlos.lopez', 'miClaveSegura789', '43.123.432');
+INSERT INTO recepcionistas (DNI, nombre, nombre_usuario, contraseÃ±a, id_creador)
+VALUES ('27.890.123', 'Laura FernÃ¡ndez', 'laura.fernandez', 'otraClaveSegura123', '21.563.213');
 
 -- REGISTRO DE CUARTOS
 INSERT INTO cuartos (numero_cuarto, disponible) VALUES (101, 1);
@@ -27,32 +25,33 @@ INSERT INTO estacionamiento (numero_lote, disponible) VALUES (423, 1);
 INSERT INTO estacionamiento (numero_lote, disponible) VALUES (556, 1);
 
 -- REGISTRO DE CLIENTES
-INSERT INTO clientes (DNI, nombre, id_creador) VALUES ('12.345.678', 'Juan Pérez', '23.432.123');
-INSERT INTO clientes (DNI, nombre, id_creador) VALUES ('98.765.432', 'Ana Martínez', '23.432.123');
-INSERT INTO clientes (DNI, nombre, id_creador) VALUES ('87.654.321', 'Luis Gómez', '56.453.213');
-INSERT INTO clientes (DNI, nombre, id_creador) VALUES ('65.432.109', 'Claudia Rodríguez', '56.453.213');
-INSERT INTO clientes (DNI, nombre, id_creador) VALUES ('54.321.098', 'Miguel Ramírez', '56.453.213');
+INSERT INTO clientes (DNI, nombre, id_creador) VALUES ('12.345.678', 'Juan PÃ©rez', '33.456.789');
+INSERT INTO clientes (DNI, nombre, id_creador) VALUES ('98.765.432', 'Ana MartÃ­nez', '33.456.789');
 
--- REGISTRO DE ESTADIA
--- registro de estadía Juan Pérez en el cuarto 101 y el lote 132
-INSERT INTO registros_estadia (DNI_cliente, numero_cuarto, numero_lote_estacionamiento, fecha_hora_entrada, fecha_hora_salida) VALUES ('12.345.678', 101, 132, '2024-10-12 14:00:00', '2024-10-13 11:00:00');
--- registro de estadía Ana Martínez en el cuarto 102 y el lote 245
-INSERT INTO registros_estadia (DNI_cliente, numero_cuarto, numero_lote_estacionamiento, fecha_hora_entrada, fecha_hora_salida) VALUES ('98.765.432', 102, 245, '2024-10-11 16:30:00', '2024-10-12 10:00:00');
--- registro de estadía Luis Gómez en el cuarto 103 y el lote 321
-INSERT INTO registros_estadia (DNI_cliente, numero_cuarto, numero_lote_estacionamiento, fecha_hora_entrada, fecha_hora_salida) VALUES ('87.654.321', 103, 321, '2024-10-13 15:00:00', '2024-10-14 09:30:00');
--- registro de estadía Claudia Rodríguez en el cuarto 104 sin uso de estacionamiento
-INSERT INTO registros_estadia (DNI_cliente, numero_cuarto, numero_lote_estacionamiento, fecha_hora_entrada, fecha_hora_salida) VALUES ('65.432.109', 104, NULL, '2024-10-10 12:00:00', '2024-10-11 10:00:00');
--- registro de estadía Miguel Ramírez en el cuarto 105 y el lote 423
-INSERT INTO registros_estadia (DNI_cliente, numero_cuarto, numero_lote_estacionamiento, fecha_hora_entrada, fecha_hora_salida) VALUES ('54.321.098', 105, 423, '2024-10-14 18:00:00', NULL);
+INSERT INTO clientes (DNI, nombre, id_creador) VALUES ('87.654.321', 'Luis GÃ³mez', '27.890.123');
+INSERT INTO clientes (DNI, nombre, id_creador) VALUES ('65.432.109', 'Claudia RodrÃ­guez', '27.890.123');
+INSERT INTO clientes (DNI, nombre, id_creador) VALUES ('54.321.098', 'Miguel RamÃ­rez', '27.890.123');
 
--- REGISTRO DE LIMPIEZA
--- limpieza cuarto 101 supervisado por Carlos López (DNI: '23.432.123')
-INSERT INTO registros_limpieza (numero_cuarto, fecha_hora_inicio, fecha_hora_fin, id_recepcionista) VALUES (101, '2024-10-12 08:00:00', '2024-10-12 09:30:00', '23.432.123');
--- limpieza cuarto 102 supervisado por María Fernández (DNI: '56.453.213')
-INSERT INTO registros_limpieza (numero_cuarto, fecha_hora_inicio, fecha_hora_fin, id_recepcionista) VALUES (102, '2024-10-13 10:00:00', '2024-10-13 11:45:00', '56.453.213');
--- limpieza cuarto 103 supervisado por Carlos López
-INSERT INTO registros_limpieza (numero_cuarto, fecha_hora_inicio, fecha_hora_fin, id_recepcionista) VALUES (103, '2024-10-14 07:30:00', '2024-10-14 09:00:00', '23.432.123');
--- limpieza cuarto 104 supervisado por María Fernández
-INSERT INTO registros_limpieza (numero_cuarto, fecha_hora_inicio, fecha_hora_fin, id_recepcionista) VALUES (104, '2024-10-14 08:00:00', '2024-10-14 09:20:00', '56.453.213');
--- limpieza cuarto 105 supervisado por Carlos López (limpieza en curso)
-INSERT INTO registros_limpieza (numero_cuarto, fecha_hora_inicio, fecha_hora_fin, id_recepcionista) VALUES (105, '2024-10-14 09:00:00', NULL, '23.432.123');
+-- REGISTRO_ESTADIA
+INSERT INTO registros_estadia (DNI_cliente, numero_cuarto, numero_lote_estacionamiento, fecha_hora_entrada, fecha_hora_salida)
+VALUES ('12.345.678', 101, 132, '2024-10-10 14:00:00', '2024-10-15 12:00:00');
+INSERT INTO registros_estadia (DNI_cliente, numero_cuarto, numero_lote_estacionamiento, fecha_hora_entrada, fecha_hora_salida)
+VALUES ('98.765.432', 102, 245, '2024-10-11 15:00:00', '2024-10-16 11:00:00');
+INSERT INTO registros_estadia (DNI_cliente, numero_cuarto, numero_lote_estacionamiento, fecha_hora_entrada, fecha_hora_salida)
+VALUES ('87.654.321', 103, 321, '2024-10-12 16:00:00', NULL);
+INSERT INTO registros_estadia (DNI_cliente, numero_cuarto, numero_lote_estacionamiento, fecha_hora_entrada, fecha_hora_salida)
+VALUES ('65.432.109', 104, 423, '2024-10-13 17:00:00', '2024-10-15 10:00:00');
+INSERT INTO registros_estadia (DNI_cliente, numero_cuarto, numero_lote_estacionamiento, fecha_hora_entrada, fecha_hora_salida)
+VALUES ('54.321.098', 105, 556, '2024-10-14 18:00:00', '2024-10-17 09:00:00');
+
+-- REGISTRO_LIMPIEZA
+INSERT INTO registros_limpieza (registro_estadia_id, id_creador)
+VALUES (1, '33.456.789');
+INSERT INTO registros_limpieza (registro_estadia_id, id_creador)
+VALUES (2, '33.456.789');
+INSERT INTO registros_limpieza (registro_estadia_id, id_creador)
+VALUES (3, '27.890.123');
+INSERT INTO registros_limpieza (registro_estadia_id, id_creador)
+VALUES (4, '27.890.123');
+INSERT INTO registros_limpieza (registro_estadia_id, id_creador)
+VALUES (5, '33.456.789');
--- SQL TP4/ExtradosQueryEjercicio4.sql
+++ SQL TP4/ExtradosQueryEjercicio4.sql
@@ -50,7 +50,7 @@ CREATE TABLE clientes (
 
 -- registros de entrada y salida
 CREATE TABLE registros_estadia (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    registro_id INT AUTO_INCREMENT PRIMARY KEY,
     DNI_cliente VARCHAR(20) NOT NULL,
     numero_cuarto INT NOT NULL,
     numero_lote_estacionamiento INT,
@@ -72,54 +72,53 @@ CREATE TABLE logs_auditoria (
 
 CREATE TABLE registros_limpieza (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    numero_cuarto INT NOT NULL,
-    fecha_hora_inicio DATETIME NOT NULL,
-    fecha_hora_fin DATETIME,
-    id_recepcionista VARCHAR(20) NOT NULL,
-    FOREIGN KEY (numero_cuarto) REFERENCES cuartos(numero_cuarto),
-    FOREIGN KEY (id_recepcionista) REFERENCES recepcionistas(DNI)
+    registro_estadia_id INT,
+    id_creador VARCHAR(20),
+    FOREIGN KEY (id_creador) REFERENCES recepcionistas (DNI), -- quien mando a que se haga la limpieza
+    FOREIGN KEY (registro_estadia_id) REFERENCES registros_estadia(registro_id)
 );
 
-CREATE TABLE registros_estadia (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    DNI_cliente VARCHAR(20) NOT NULL,
-    numero_cuarto INT NOT NULL,
-    numero_lote_estacionamiento INT,
-    fecha_hora_entrada DATETIME NOT NULL, -- momento en el que el cliente ingresa al cuarto
-    fecha_hora_salida DATETIME,
-    FOREIGN KEY (DNI_cliente) REFERENCES clientes(DNI),
-    FOREIGN KEY (numero_cuarto) REFERENCES cuartos(numero_cuarto),
-    FOREIGN KEY (numero_lote_estacionamiento) REFERENCES estacionamiento(numero_lote)
-);
+CREATE USER 'administrador'@'localhost' IDENTIFIED BY 'admin';
+CREATE USER 'recepcionista'@'localhost' IDENTIFIED BY 'recepcionista_password';
+
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'administrador'@'localhost';
+REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'recepcionista'@'localhost';
+
+GRANT ALL PRIVILEGES ON TP4.* TO 'administrador'@'localhost';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON TP4.recepcionistas TO 'recepcionista'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON TP4.cuartos TO 'recepcionista'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON TP4.estacionamiento TO 'recepcionista'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON TP4.clientes TO 'recepcionista'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON TP4.registros_estadia TO 'recepcionista'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON TP4.registros_limpieza TO 'recepcionista'@'localhost';
+
+GRANT EXECUTE ON TP4.* TO 'administrador'@'localhost';
+GRANT EXECUTE ON TP4.* TO 'recepcionista'@'localhost';
+
+FLUSH PRIVILEGES;
 
  -- ACTIVIDAD 1
 CREATE VIEW registrar_movimientos_clientes AS
 SELECT reg.DNI_cliente, c.nombre AS nombre_cliente, reg.numero_cuarto, reg.fecha_hora_entrada, reg.fecha_hora_salida,
 TIMESTAMPDIFF(HOUR, fecha_hora_entrada, fecha_hora_salida) AS horas_de_diferencia
 FROM registros_estadia reg
-INNER JOIN clientes c ON c.DNI = reg.DNI_cliente
+INNER JOIN clientes c ON c.DNI = reg.DNI_cliente;
 
--- ACTIVIDAD 2
+-- ACTIVIDAD 3
+CREATE VIEW limpieza_cuarto AS
+SELECT r.numero_cuarto AS Numero_Cuarto,
+       r.DNI_cliente AS DNI_Cliente,
+       r.fecha_hora_salida + INTERVAL 30 MINUTE AS Inicio_Limpieza,
+       r.fecha_hora_salida + INTERVAL 120 MINUTE AS Fin_Limpieza,
+       l.id_creador AS DNI_Recepcionista,
+       rec.nombre as Nombre_Recepcionista
+FROM registros_limpieza l
+INNER JOIN registros_estadia r 
+ON l.registro_estadia_id = r.registro_id
+INNER JOIN recepcionistas rec
+ON l.id_creador = rec.DNI;
 
-CREATE USER 'admin_usuario'@'localhost' IDENTIFIED BY 'admin_password';
-CREATE USER 'recepcionista_usuario'@'localhost' IDENTIFIED BY 'recepcionista_password';
-
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'admin_usuario'@'localhost';
-REVOKE ALL PRIVILEGES, GRANT OPTION FROM 'recepcionista_usuario'@'localhost';
-
--- permisos de administración (CRUD + DDL)
-GRANT ALL PRIVILEGES ON TP4.* TO 'admin_usuario'@'localhost';
-
--- permisos CRUD
-GRANT SELECT, INSERT, UPDATE, DELETE ON TP4.* TO 'recepcionista_usuario'@'localhost';
-
--- _sp
-GRANT EXECUTE ON TP4.* TO 'admin_usuario'@'localhost';
-GRANT EXECUTE ON TP4.* TO 'recepcionista_usuario'@'localhost';
-
-GRANT CREATE, DROP, ALTER, CREATE USER ON *.* TO 'admin_usuario'@'localhost';
-
-FLUSH PRIVILEGES;
 
 
 
